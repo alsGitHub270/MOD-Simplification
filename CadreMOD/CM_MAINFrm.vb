4aaea82409d52537b160282b5321a14f1cb26f04
Al Adolfsen <aadolfsen@yahoo.com>
2017-06-30 12:48:20 -0400

    Updated Add bank to use Drop Down

--- CadreMOD/CM_MAINFrm.vb
+++ CadreMOD/CM_MAINFrm.vb
@@ -8,7 +8,7 @@ Imports System.Drawing
 Imports System.Globalization
 Imports System.IO
 Imports System.Windows.Forms
-
+Imports System.Reflection
 
 Partial Friend Class CM_MAIN_frm
     Inherits System.Windows.Forms.Form
@@ -70,6 +70,7 @@ Partial Friend Class CM_MAIN_frm
     End Sub
 
     Private Sub CM_MAIN_frm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
         Dim ShiftDist As Single = (Me.Width - BuildingInformation_fra.Width) / 2
 
         BuildingInformation_fra.Left = ShiftDist
@@ -89,6 +90,7 @@ Partial Friend Class CM_MAIN_frm
         FpSpread1.HorizontalScrollBar.Renderer = Nothing
         FpSpread1.VerticalScrollBar.Renderer = Nothing
         FpSpread1.VisualStyles = FarPoint.Win.VisualStyles.Off
+
 
         'Init Spread
         With FpSpread1.ActiveSheet
@@ -404,7 +406,6 @@ Partial Friend Class CM_MAIN_frm
 
     Private Sub btnMerge_Click(sender As System.Object, e As System.EventArgs) Handles btnMerge.Click
 
-        Dim active_row As Array
         Dim new_message As String = "You are about to merge data for Bank A."
         Dim update_message As String = "You are about to update 'Master' data for Bank A.  All current data for the Master will be overwritten."
 
@@ -511,7 +512,7 @@ Partial Friend Class CM_MAIN_frm
         printset.PdfWriteMode = FarPoint.Win.Spread.PdfWriteMode.Append
 
         ' assign the printer settings and print
-        '''FpSpread1.Sheets().Printinfo = printset
+        'FpSpread1.Sheets().Printinfo = printset
 
 
         ' child view
@@ -595,72 +596,42 @@ Partial Friend Class CM_MAIN_frm
 
         Dim cbstr As String()
         cbstr = New String() {"Geared", "Gearless", "Hydro"}
-        Dim cmbocell As New FarPoint.Win.Spread.CellType.ComboBoxCellType()
-        cmbocell.Items = cbstr
-        cmbocell.AutoSearch = FarPoint.Win.AutoSearch.SingleCharacter
-        cmbocell.Editable = False
-        cmbocell.MaxDrop = 3
-        FpSpread1.ActiveSheet.Cells(FpSpread1.ActiveSheet.ActiveRowIndex, 4).CellType = cmbocell
+        Dim cmbocell_machine As New FarPoint.Win.Spread.CellType.ComboBoxCellType()
+        cmbocell_machine.Items = cbstr
+        cmbocell_machine.AutoSearch = FarPoint.Win.AutoSearch.SingleCharacter
+        cmbocell_machine.Editable = False
+        cmbocell_machine.MaxDrop = 4
+        FpSpread1.ActiveSheet.Cells(FpSpread1.ActiveSheet.ActiveRowIndex, 4).CellType = cmbocell_machine
 
-        Dim valid As Boolean = False
-
+        Dim cbstr1 As String()
         FpSpread1.ActiveSheet.SetActiveCell(FpSpread1.ActiveSheet.ActiveRowIndex, 3, False)
+        cbstr1 = BuildAvailableBanks()
+        Dim cmbocell_bank As New FarPoint.Win.Spread.CellType.ComboBoxCellType()
+        cmbocell_bank.Items = cbstr1
+        cmbocell_bank.AutoSearch = FarPoint.Win.AutoSearch.SingleCharacter
+        cmbocell_bank.Editable = False
+        cmbocell_bank.MaxDrop = 4
+        FpSpread1.ActiveSheet.Cells(FpSpread1.ActiveSheet.ActiveRowIndex, 3).CellType = cmbocell_bank
+
+
 
 
         FpSpread1.Refresh()
     End Sub
 
-    Private Function ValidBank() As Boolean
-        Dim valid As Boolean = False
+   
 
-        Dim cell_value As String
-        cell_value = FpSpread1.ActiveSheet.Cells(FpSpread1.ActiveSheet.ActiveRowIndex, 3).Value.ToString.ToUpper
-        FpSpread1.ActiveSheet.Cells(FpSpread1.ActiveSheet.ActiveRowIndex, 3).Value = cell_value
-
-        If cell_value = "" Then
-            MessageBox.Show("You must enter a Bank", "Please enter the Bank")
-            valid = False
-        ElseIf BankExists(cell_value) Then
-            MessageBox.Show("The Bank you entered already exists", "Duplicated Bank")
-            valid = False
-        Else
-            FpSpread1.ActiveSheet.Cells(FpSpread1.ActiveSheet.ActiveRowIndex, 1).Value = cell_value + "1"
-            valid = True
-        End If
-
-        Return valid
-    End Function
-
-    Private Function BankExists(cell_value As String) As Boolean
-
-        Dim bank_exists As Boolean = False
-
-        'With FpSpread1.ActiveSheet
-        '    For i As Integer = 0 To .RowCount - 1 'Assuming the new row is on the last row, and should not be interrogated
-        '        If .Cells(i, 3).Value = cell_value Then
-        '            bank_exists = True
-        '            Exit For
-        '        End If
-        '    Next
-        'End With
-
-        Return bank_exists
-    End Function
-
-    Private Sub FpSpread1_Click(sender As Object, e As System.EventArgs) Handles FpSpread1.Click
-
-        Stop
-
-    End Sub
 
     Private Sub FpSpread1_LeaveCell(sender As Object, e As FarPoint.Win.Spread.LeaveCellEventArgs) Handles FpSpread1.LeaveCell
 
-        Select e.Column
-            Case 3          ' bank
-                If Not ValidBank() Then
-                    e.Cancel = True
-                End If
-                FpSpread1.ActiveSheet.SetActiveCell(FpSpread1.ActiveSheet.ActiveRowIndex, 4, False)
+        Dim textBoxType As New FarPoint.Win.Spread.CellType.TextCellType()
+
+        Select Case e.Column
+            'Case 3          ' bank
+            '    If Not ValidBank() Then
+            '        e.Cancel = True
+            '    End If
+            '    FpSpread1.ActiveSheet.SetActiveCell(FpSpread1.ActiveSheet.ActiveRowIndex, 4, False)
             Case 4
                 If IsNothing(FpSpread1.ActiveSheet.Cells(FpSpread1.ActiveSheet.ActiveRowIndex, 4).Value) Then
                     MessageBox.Show("Please select a machine.", "Missing Data")
@@ -676,7 +647,12 @@ Partial Friend Class CM_MAIN_frm
         If e.Cancel = False Then
             If e.Row <> e.NewRow Then
                 If StillValid() Then
-                    FpSpread1.ActiveSheet.SortRows(3, True, True)
+                    With FpSpread1.ActiveSheet
+                        For i As Integer = 0 To .RowCount - 1 'Assuming the new row is on the last row, and should not be interrogated
+                            .Cells(i, 3).CellType = textBoxType
+                        Next
+                        .SortRows(3, True, True)
+                    End With
                     FpSpread1.Refresh()
                 Else
                     e.Cancel = True
@@ -724,10 +700,6 @@ Partial Friend Class CM_MAIN_frm
     Private Function StillValid() As Boolean
         Dim isValid As Boolean = True
 
-        If Not ValidBank() Then
-            isValid = False
-        End If
-
         If IsNothing(FpSpread1.ActiveSheet.Cells(FpSpread1.ActiveSheet.ActiveRowIndex, 4).Value) Or FpSpread1.ActiveSheet.Cells(FpSpread1.ActiveSheet.ActiveRowIndex, 4).Value.ToString.Trim = "" Then
             MessageBox.Show("Please select a machine.", "Missing Data")
             isValid = False
@@ -758,8 +730,6 @@ Partial Friend Class CM_MAIN_frm
         Else
             DeleteAltRow(activeRows)
         End If
-        'Stop
-
 
     End Sub
 
@@ -824,11 +794,47 @@ Partial Friend Class CM_MAIN_frm
     End Sub
 
     Private Sub btnDeleteMaster_Click(sender As System.Object, e As System.EventArgs) Handles btnDeleteMaster.Click
-        Dim ChildSheetView1 As FarPoint.Win.Spread.SheetView = Nothing
-        ChildSheetView1 = FpSpread1.ActiveSheet.FindChildView(0, 0)
-        If ChildSheetView1.RowCount > 1 Then
-            ChildSheetView1.RemoveRows(0, 1)
+
+        If MessageBox.Show("You are about to delete the Master for this Summary Row.  Are you Sure", "Are you Sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) = DialogResult.Yes Then
+            Dim ChildSheetView1 As FarPoint.Win.Spread.SheetView = Nothing
+            ChildSheetView1 = FpSpread1.ActiveSheet.FindChildView(0, 0)
+            If ChildSheetView1.RowCount > 1 Then
+                ChildSheetView1.RemoveRows(0, 1)
+            End If
+
+            Dim p As New FarPoint.Win.Picture(Image.FromFile("\images\openned.png"), FarPoint.Win.RenderStyle.Normal)
+            Dim t As New FarPoint.Win.Spread.CellType.TextCellType
+            t.BackgroundImage = p
+            ' Apply the text cell.
+            FpSpread1.ActiveSheet.Cells(0, 2).CellType = t
         End If
 
+
     End Sub
+
+    Private Function BuildAvailableBanks() As String()
+        ' Roll thru all the banks in the summary row.  Add to string if not available
+       
+        Dim myList As New List(Of String)()
+        Dim i As Integer
+        Dim bank As String
+
+        For i = 65 To 90     'build the initial list (A...Z)
+            myList.Add(Chr(i))
+        Next
+
+        For Each dr As DataRow In myDataSet.Tables(0).Rows
+            bank = dr("Bank")
+            If bank <> "" Then
+                i = myList.IndexOf(bank)
+                myList.RemoveAt(i)
+            End If
+        Next
+
+        Dim thisString() As String = myList.ToArray()
+
+        Return thisString
+
+    End Function
+
 End Class
